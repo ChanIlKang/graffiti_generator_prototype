5c9490ddd3b37400f776baced6976cc123843018
KangChanil <cikang@eqqui.com>
2025-04-27 13:36:19 +0900

    feat: prepare to deploy gh-pages

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/리포지토리명/', // <<<<<<<< 이거 추가!!
+  base: '/graffiti_generator_prototype/',
   plugins: [
     vue(),
     vueDevTools(),
